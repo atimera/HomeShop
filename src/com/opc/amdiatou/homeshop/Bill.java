68d8044bd63081bb6af51fd77f2b9fa65185da97
laenzo <amdiatoutimera@hotmail.fr>
2018-09-13 14:33:20 +0200

    add Constructor for Bill, Customer and Product
    attributs for Bill: customer, Map<product, quantity>

--- src/com/opc/amdiatou/homeshop/Bill.java
+++ src/com/opc/amdiatou/homeshop/Bill.java
@@ -1,4 +1,29 @@
 package com.opc.amdiatou.homeshop;
 
+import java.util.Map;
+
 public class Bill {
+    private Customer customer;
+    private Map<Product, Integer> product;
+
+    public Bill(Customer customer){
+        this.customer = customer;
+    }
+
+    /**
+     * Add a product with a quantity in the bill
+     * @param product product to add
+     * @param quantity quantity of the product
+     */
+    public void addProduct(Product product, Integer quantity){
+
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Map<Product, Integer> getProduct() {
+        return product;
+    }
 }
