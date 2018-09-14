f77e417aaa8f1a40c56b5d9495ad34af64f97574
laenzo <amdiatoutimera@hotmail.fr>
2018-09-14 03:20:12 +0200

    Generating a web page with a form using BillServlet Class

--- src/main/java/com/opc/amdiatou/homeshop/web/HelloWorldServlet.java
+++ src/main/java/com/opc/amdiatou/homeshop/web/HelloWorldServlet.java
@@ -9,6 +9,6 @@ import java.io.IOException;
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Hello OKOK world! I'm Hamza");
+        resp.getWriter().println("Hello world! I'm Hamza");
     }
 }
